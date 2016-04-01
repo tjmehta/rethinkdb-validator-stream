e68618d41d05484d762fba937d07ea5e8ff765ab
tjmehta <tejesh.mehta@gmail.com>
2016-03-31 17:09:58 -0700

    handshake support fix

--- test/create-validator-stream.test.js
+++ test/create-validator-stream.test.js
@@ -1,5 +1,6 @@
 if (typeof Promise === 'undefined') require('es6-promise').polyfill()
 // var createResponseChunker = require('rethinkdb-stream-chunker').createResponseStreamChunker
+var createCount = require('callback-count')
 var expect = require('chai').expect
 var pick = require('101/pick')
 var r = require('rethinkdb')
@@ -28,6 +29,68 @@ describe('create-validator-stream tests', function () {
         self.socket = connection.rawSocket
         done()
       })
+    })
+
+    it('should passthrough handshake', function (done) {
+      var opts = {
+        whitelist: [
+          r.table('test-table').get('hey')
+        ],
+        db: this.db
+      }
+      var handshakeBuf = new Buffer([ 0x20, 0x2d, 0x0c, 0x40, 0x00, 0x00, 0x00, 0x00, 0xc7, 0x70, 0x69, 0x7e ])
+      this.socket.writeStream.pipe(createValidator(opts, false)).on('data', function (_handshakeBuf) {
+        try {
+          expect(_handshakeBuf).to.deep.equal(handshakeBuf)
+          done()
+        } catch (err) {
+          done(err)
+        }
+      })
+      this.socket.writeStream.write(handshakeBuf)
+    })
+
+    it('should passthrough handshake and then a query w/ expected opts', function (done) {
+      var self = this
+      var query = r.table('test-table').get('hey')
+      var opts = {
+        whitelist: [
+          r.table('test-table').get('hey')
+        ],
+        db: this.db
+      }
+      var handshakeBuf = new Buffer([ 0x20, 0x2d, 0x0c, 0x40, 0x00, 0x00, 0x00, 0x00, 0xc7, 0x70, 0x69, 0x7e ])
+      var expectedChunks = [
+        // handshake
+        function (buf) {
+          expect(buf).to.deep.equal(handshakeBuf)
+        },
+        // query
+        function (buf) {
+          var queryParts = pick(parseQueryBuffer(buf), ['type', 'term', 'opts'])
+          expect(queryParts).to.deep.equal({
+            type: 1,
+            term: query.build(),
+            opts: {
+              db: r.db(self.db).build()
+            }
+          })
+        }
+      ]
+      var i = 0
+      this.socket.writeStream.pipe(createValidator(opts, false)).on('data', function (chunkBuf) {
+        try {
+          expectedChunks[i](chunkBuf)
+          i++
+          if (i === 2) {
+            done()
+          }
+        } catch (err) {
+          done(err)
+        }
+      })
+      this.socket.writeStream.write(handshakeBuf)
+      query.run(this.connection, function () {})
     })
 
     it('should passthrough queries w/ expected opts', function (done) {
@@ -87,18 +150,54 @@ describe('create-validator-stream tests', function () {
       })
     })
 
+    it('should passthrough handshake and then an error for query w/ unexpected opts', function (done) {
+      var next = createCount(2, done).next
+      var query = r.table('test-table').get('hey')
+      var opts = {
+        whitelist: [
+          r.table('test-table').get('hey')
+        ],
+        db: 'unexpected-db' // mismatch!
+      }
+      var handshakeBuf = new Buffer([ 0x20, 0x2d, 0x0c, 0x40, 0x00, 0x00, 0x00, 0x00, 0xc7, 0x70, 0x69, 0x7e ])
+      this.socket.writeStream.pipe(createValidator(opts, false))
+        .on('data', function (chunkBuf) {
+          try {
+            expect(chunkBuf).to.deep.equal(handshakeBuf)
+            next()
+          } catch (err) {
+            next(err)
+          }
+        })
+        .on('error', function (err) {
+          try {
+            expect(err).to.be.an.instanceOf(ValidationError)
+            expect(err.message).to.equal('"opts" mismatch')
+            next()
+          } catch (err) {
+            next(err)
+          }
+        })
+      this.socket.writeStream.write(handshakeBuf)
+      query.run(this.connection, function () {})
+    })
+
     it('should error for queries w/ unexpected opts', function (done) {
       var query = r.table('test-table').get('hey')
       var opts = {
         whitelist: [
           r.table('test-table').get('hey')
         ],
-        db: 'expected-db' // mismatch!
+        db: 'unexpected-db' // mismatch!
       }
       this.socket.writeStream.pipe(createValidator(opts, true)).on('error', function (err) {
-        expect(err).to.be.an.instanceOf(ValidationError)
-        expect(err.message).to.equal('"opts" mismatch')
-        done()
+        try {
+          expect(err).to.be.an.instanceOf(ValidationError)
+          expect(err.message).to.equal('"opts" mismatch')
+          done()
+        } catch (err) {
+          done(err)
+        }
       })
       query.run(this.connection, function () {})
     })
